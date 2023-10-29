917c53350552003d31e75461751cc6e92be82487
yoyom261 <116827654+yoyom261@users.noreply.github.com>
2023-10-29 11:34:36 +0100

    init

---
+++ JeuDuPendu/FrmPendu.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace JeuDuPendu
+{
+    partial class frmPendu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMot = new System.Windows.Forms.TextBox();
+            this.lblResultat = new System.Windows.Forms.Label();
+            this.grpTestLettres = new System.Windows.Forms.GroupBox();
+            this.lblLettres = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.cboLettre = new System.Windows.Forms.ComboBox();
+            this.btnRejouer = new System.Windows.Forms.Button();
+            this.imgPendu = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.grpTestLettres.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPendu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMot);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(376, 80);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "mot à chercher";
+            // 
+            // txtMot
+            // 
+            this.txtMot.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMot.Location = new System.Drawing.Point(8, 23);
+            this.txtMot.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMot.MaxLength = 15;
+            this.txtMot.Name = "txtMot";
+            this.txtMot.Size = new System.Drawing.Size(331, 47);
+            this.txtMot.TabIndex = 0;
+            this.txtMot.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMot_KeyPress);
+            // 
+            // lblResultat
+            // 
+            this.lblResultat.AutoSize = true;
+            this.lblResultat.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultat.Location = new System.Drawing.Point(16, 283);
+            this.lblResultat.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResultat.Name = "lblResultat";
+            this.lblResultat.Size = new System.Drawing.Size(0, 39);
+            this.lblResultat.TabIndex = 4;
+            // 
+            // grpTestLettres
+            // 
+            this.grpTestLettres.Controls.Add(this.lblLettres);
+            this.grpTestLettres.Controls.Add(this.btnTest);
+            this.grpTestLettres.Controls.Add(this.cboLettre);
+            this.grpTestLettres.Location = new System.Drawing.Point(16, 102);
+            this.grpTestLettres.Margin = new System.Windows.Forms.Padding(4);
+            this.grpTestLettres.Name = "grpTestLettres";
+            this.grpTestLettres.Padding = new System.Windows.Forms.Padding(4);
+            this.grpTestLettres.Size = new System.Drawing.Size(376, 137);
+            this.grpTestLettres.TabIndex = 3;
+            this.grpTestLettres.TabStop = false;
+            this.grpTestLettres.Text = "lettres testées";
+            // 
+            // lblLettres
+            // 
+            this.lblLettres.AutoSize = true;
+            this.lblLettres.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLettres.Location = new System.Drawing.Point(8, 78);
+            this.lblLettres.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLettres.Name = "lblLettres";
+            this.lblLettres.Size = new System.Drawing.Size(350, 28);
+            this.lblLettres.TabIndex = 2;
+            this.lblLettres.Text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(88, 23);
+            this.btnTest.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(77, 37);
+            this.btnTest.TabIndex = 1;
+            this.btnTest.Text = "TEST";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // cboLettre
+            // 
+            this.cboLettre.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboLettre.FormattingEnabled = true;
+            this.cboLettre.Location = new System.Drawing.Point(8, 23);
+            this.cboLettre.Margin = new System.Windows.Forms.Padding(4);
+            this.cboLettre.Name = "cboLettre";
+            this.cboLettre.Size = new System.Drawing.Size(71, 35);
+            this.cboLettre.TabIndex = 0;
+            // 
+            // btnRejouer
+            // 
+            this.btnRejouer.Image = global::JeuDuPendu.Properties.Resources.playagain;
+            this.btnRejouer.Location = new System.Drawing.Point(331, 266);
+            this.btnRejouer.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRejouer.Name = "btnRejouer";
+            this.btnRejouer.Size = new System.Drawing.Size(61, 57);
+            this.btnRejouer.TabIndex = 2;
+            this.btnRejouer.UseVisualStyleBackColor = true;
+            this.btnRejouer.Click += new System.EventHandler(this.btnRejouer_Click);
+            // 
+            // imgPendu
+            // 
+            this.imgPendu.ErrorImage = null;
+            this.imgPendu.Image = global::JeuDuPendu.Properties.Resources.pendu0;
+            this.imgPendu.Location = new System.Drawing.Point(405, 15);
+            this.imgPendu.Margin = new System.Windows.Forms.Padding(4);
+            this.imgPendu.Name = "imgPendu";
+            this.imgPendu.Size = new System.Drawing.Size(333, 308);
+            this.imgPendu.TabIndex = 1;
+            this.imgPendu.TabStop = false;
+            // 
+            // frmPendu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 334);
+            this.Controls.Add(this.lblResultat);
+            this.Controls.Add(this.grpTestLettres);
+            this.Controls.Add(this.btnRejouer);
+            this.Controls.Add(this.imgPendu);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmPendu";
+            this.Text = "Jeu du Pendu";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.grpTestLettres.ResumeLayout(false);
+            this.grpTestLettres.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPendu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtMot;
+        private System.Windows.Forms.PictureBox imgPendu;
+        private System.Windows.Forms.Button btnRejouer;
+        private System.Windows.Forms.Label lblResultat;
+        private System.Windows.Forms.GroupBox grpTestLettres;
+        private System.Windows.Forms.Label lblLettres;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.ComboBox cboLettre;
+    }
+}
+
---
+++ JeuDuPendu/FrmPendu.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+namespace JeuDuPendu
+{
+    public partial class frmPendu : Form
+    {
+        /// <summary>
+        /// mot à rechercher
+        /// </summary>
+        private string mot;
+        /// <summary>
+        /// étape d'affichage du pendu
+        /// </summary>
+        private int etapePendu;
+        /// <summary>
+        /// maximum d'étapes du pendu
+        /// </summary>
+        private int maxPendu = 10;
+
+
+        /// <summary>
+        /// Initialisation des objets graphiques
+        /// </summary>
+        public frmPendu()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Rempli le combo avec les lettres de l'alphabet
+        /// </summary>
+        private void RemplirCboLettres()
+        {
+            // vide le combo
+            cboLettre.Items.Clear();
+            // rempli le combo avec les lettres de l'alphabet
+            for (int k = 0; k < 26; k++)
+            {
+                cboLettre.Items.Add((char)('A' + k));
+            }
+            cboLettre.SelectedIndex = 0;
+        }
+        /// <summary>
+        /// Préparation de la phase 1 (saisie du mot à chercher)
+        /// </summary>
+        private void PreparationPhase1()
+        {
+            // réinitialise l'étape du pendu et affiche l'image vide
+            etapePendu = 0;
+            AfficheImage(etapePendu);
+            // désactive la zone de la phase 2 (proposition de lettres)
+            grpTestLettres.Enabled = false;
+            // vide le label des lettres
+            lblLettres.Text = "";
+            // supprime le message
+            lblResultat.Text = "";
+            // active, vide et se positionne sur la zone de saisie du mot
+            txtMot.Enabled = true;
+            txtMot.Text = "";
+            txtMot.Focus();
+        }
+        /// <summary>
+        /// Préparation de la phase 2 (recherche du mot)
+        /// </summary>
+        private void PreparationPhase2()
+        {
+            // active la zone de la phase 2 (proposition de lettres)
+            grpTestLettres.Enabled = true;
+            // désactive la zone de texte contenant le mot
+            txtMot.Enabled = false;
+            // remplit le combo des lettres
+            RemplirCboLettres();
+            // donne le focus au bouton test
+            btnTest.Focus();
+        }
+
+        /// <summary>
+        /// Affiche une image d'une numéro précis
+        /// </summary>
+        /// <param name="num"></param>
+        private void AfficheImage(int num)
+        {
+            imgPendu.Image = (Image)Properties.Resources.ResourceManager.GetObject("pendu" + num);
+        }
+
+        /// <summary>
+        /// Afichage d'une étape du pendu
+        /// </summary>
+        /// <returns></returns>
+        private bool AffichePendu()
+        {
+            etapePendu++;
+            AfficheImage(etapePendu);
+            return (etapePendu == maxPendu);
+        }
+
+        /// <summary>
+        /// Recherche la lettre dans le mot et remplace le tiret par la lettre
+        /// retourne vrai si la lettre est trouvée au moins une fois
+        /// </summary>
+        /// <param name="lettre">lettre à chercher</param>
+        /// <returns></returns>
+        private bool RechercheLettreDansMot(char lettre)
+        {
+            int position = -1; // position de la lettre dans le mot
+            bool trouve = false; // pour savoir si la lettre a été trouvée
+            // boucle sur la recherche de la lettre (qui peut être présente plusieurs fois)
+            do
+            {
+                // récupère la position de la lettre (ou -1)
+                position = mot.IndexOf(lettre, position + 1);
+                if (position != -1)
+                {
+                    trouve = true;
+                    // remplace le tiret par la lettre dans la zone de texte
+                    txtMot.Text = txtMot.Text.Remove(position, 1);
+                    txtMot.Text = txtMot.Text.Insert(position, lettre.ToString());
+                }
+            } while (position != -1);
+            return trouve;
+        }
+
+        /// <summary>
+        /// Traitement de la lettre récupérée
+        /// </summary>
+        /// <param name="lettre"></param>
+        private void TraitementLettre(char lettre)
+        {
+            // cherche si la lettre est présente dans le mot
+            if (!RechercheLettreDansMot(lettre))
+            {
+                // lettre non trouvée : affichage du pendu
+                if (AffichePendu())
+                {
+                    // si totalement pendu, perdu et fin du jeu
+                    FinJeu("PERDU");
+                }
+            }
+            else
+            {
+                // il n'y a plus de lettre à trouver
+                if (txtMot.Text.IndexOf('-') == -1)
+                {
+                    // s'il n'y a plus de tiret (toutes les lettres trouvées) c'est gagné
+                    FinJeu("GAGNE");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Fin du jeu (gagné ou perdu)
+        /// </summary>
+        /// <param name="message"></param>
+        private void FinJeu(string message)
+        {
+            // affichage du message (gagné ou perdu)
+            lblResultat.Text = message;
+            // affiche le mot correct
+            txtMot.Text = mot;
+            // désactive la zone de proposition de lettre
+            grpTestLettres.Enabled = false;
+            // se positionne sur le bouton pour recommencer
+            btnRejouer.Focus();
+        }
+
+        /// <summary>
+        /// Contrôle si un mot est bien constitué uniquement de lettres
+        /// </summary>
+        /// <param name="unMot"></param>
+        /// <returns></returns>
+        private bool MotCorrect(string unMot)
+        {
+            unMot = unMot.ToUpper();
+            bool correct = true;
+            for (int k = 0; k < unMot.Length; k++)
+            {
+                //   if (char.Parse(unMot.Substring(k, 1)) < 'A' || char.Parse(unMot.Substring(k, 1)) > 'Z')
+                if (unMot[k] < 'A' || unMot[k] > 'Z')
+                {
+                    correct = false;
+                }
+            }
+            return correct;
+        }
+        /// <summary>
+        /// Evénement chargement de la fenêtre
+        /// création des boutons des lettres de l'alphabet
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmPendu_Load(object sender, EventArgs e)
+        {
+            // préparation des objets graphiques pour la phase 1 (saisie du mot)
+            PreparationPhase1();
+        }
+
+        /// <summary>
+        /// Evénement touche enfoncée dans la zone du mot
+        /// si validation, début de la phase 2
+        /// si validation, enregistrement du mot et début du jeu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtMot_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // validation donc fin de la saisie du mot
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                // vérifie qu'un mot a été tapé et qu'il est correct (uniquement lettres)
+                if (!txtMot.Text.Equals("") && MotCorrect(txtMot.Text))
+                {
+                    // met le mot en majuscule et le mémorise dans une propriété
+                    mot = txtMot.Text.ToUpper();
+                    // remplit la zone de tirets à la place des lettres
+                    txtMot.Text = "";
+                    for (int k = 0; k < mot.Length; k++)
+                    {
+                        txtMot.Text += "-";
+                    }
+                    // préparation des objets graphiques pour la phase 2 (recherche du mot)
+                    PreparationPhase2();
+                }
+                else
+                {
+                    // le mot n'est pas correct : efface la zone
+                    MessageBox.Show("Le mot ne doit comporter que des lettres alphabétiques (pas d'espace, pas d'accent)");
+                    txtMot.Text = "";
+                    txtMot.Focus();
+                }
+            }
+        }
+        /// <summary>
+        /// Evénement clic sur le bouton rejouer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRejouer_Click(object sender, EventArgs e)
+        {
+            // préparation des objets graphiques pour la phase 1 (saisie du mot)
+            PreparationPhase1();
+        }
+        /// <summary>
+        /// Evénement clic sur le bouton test
+        /// recherche de la lettre dans le mot
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // récupération de la lettre, affichage et suppression dans le combo
+                char lettre = char.Parse(cboLettre.SelectedItem.ToString());
+                lblLettres.Text += lettre;
+                cboLettre.Items.RemoveAt(cboLettre.SelectedIndex);
+                // traite la lettre (recherche si elle est présente dans le mot)
+                TraitementLettre(lettre);
+                // se positionne sur la 1ère lettre du combo
+                cboLettre.SelectedIndex = 0;
+            }
+            catch
+            {
+                // cas d'une tentative de suppression de lettre alors que le combo est vide
+                // normalement ce cas ne doit pas arriver car le mot doit être trouvé avant
+                grpTestLettres.Enabled = false;
+            }
+        }
+        /// <summary>
+        /// Evénement choix d'une lettre dans le combo
+        /// position du focus sur le bouton  test
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cboLettre_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnTest.Focus();
+        }
+
+    }
+}
